4b1be1c5c6f51c840c97536ac0470162e88a5d50
seblaredo <38118181+seblaredo@users.noreply.github.com>
2026-06-23 10:38:04 -0400

    Add button functionality and operations
    Can now calculate with all operators and clear the screen when needed

--- script.js
+++ script.js
@@ -1,51 +1,91 @@
 let a = null;
-let oper;
-let b;
+let op = null;
+let b = null;
+let lastCalled = null;
 const display = document.querySelector(".screen p");
 const keypad = document.querySelector(".keypad"); 
 
 function add(a, b){
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function substract (a, b){
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply(a, b){
-    return a*b;
+    return Number(a)*Number(b);
 }
 
 function divide(a, b){
     let answer;
-    b === 0 ? answer = "Not possible!" : answer = a/b;
+    b === "0" ? answer = "Not possible!" : answer = Number(a)/Number(b);
     return answer;
 }
 
-function operate(a, oper, b){
+function operate(a, op, b){
     let result = 0;
-    switch (oper){
-        case '+': result = add(a, b);
-        case '-': result = substract(a, b);
-        case '*': result = multiply(a, b);
-        case '/': result = divide(a, b);
+    switch (op){
+        case '+': result = add(a, b); break;
+        case '-': result = substract(a, b); break;
+        case '*': result = multiply(a, b); break;
+        case '/': result = divide(a, b); break;
     }
     return result;
 }
 
-function updateNum(num){
-    if(a === null){
-        a = num;
-    } else {
-        b = num;
-    }
-    display.textContent = a;
+function addNum(num){
+    display.textContent += num;
+}
+
+function displayNum(num){
+    display.textContent = num;
+}
+
+function allClear(){
+    console.log("clear!");
+    display.textContent = "";
+    a = null;
+    op = null;
+    b = null;
+    lastCalled = null;
 }
 
 keypad.addEventListener("click", (e)=>{
     let target = e.target;
     let number = "";
-    switch(target.id){
-        case "1": updateNum(1);
+    if(target.classList.contains("op")){
+        console.log("operator!");
+        if(lastCalled === "num" && op === null){
+            op = target.id;
+            lastCalled = "op";
+            a = display.textContent;
+        } else if (lastCalled === "num" && op != null){
+            b = display.textContent;
+            display.textContent = operate(a, op, b);
+            a = display.textContent;
+            op = target.id;
+            lastCalled = "op";
+        } else if (lastCalled === "op"){
+            op = target.id;
+        }
+    } else if (target.classList.contains("num")){
+        console.log("num!");
+        if(lastCalled === null || lastCalled === "num"){
+            addNum(target.id);
+            lastCalled = "num";
+        } else if (lastCalled === "op"){
+            display.textContent = "";
+            addNum(target.id);
+            lastCalled = "num";
+        }
+    } else if(target.classList.contains("clear")){
+        allClear();
+    } else if(target.classList.contains("equal")){
+        b = display.textContent;
+        display.textContent = operate(a, op, b);
+        a = display.textContent;
+        op = null;
+        lastCalled = "op";
     }
 })
